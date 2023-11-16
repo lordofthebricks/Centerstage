4a1ffd80205e593dc5a0b468b10ef3d1a2cb8cf6
lordogthebricks <Graham!12>
2023-11-15 19:25:01 -0500

    finished autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/noahAutonomous1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/noahAutonomous1.java
@@ -15,11 +15,9 @@ public class noahAutonomous1 extends LinearOpMode {
         robot.init(hardwareMap);
         waitForStart();
 
-        robot.encoderDrive(0.6,24,24,24,24, 3);
-        robot.encoderDrive(0.6,-28, 28, 28, -28, 3);
-        robot.encoderDrive(0.6, 72, 72, 72, 72,3);
-        robot.armControl(0.8, 190);
-        robot.armControl(0.8,0);
+       robot.encoderDrive(0.6,hardware.TILE * 4,-hardware.TILE * 4, hardware.TILE*4,-hardware.TILE * 4,3);
+
+
     }
 }
 
