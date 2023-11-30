72efaad8345f83a02ec7f8064856ef5a04e10685
noah7564 <noahjackson@grovechristianschool.com>
2023-11-29 21:15:48 -0500

    finished the arm movement autonomouse

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmMovementBlue1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmMovementBlue1.java
@@ -13,16 +13,17 @@ public class ArmMovementBlue1 extends LinearOpMode {
         robot.init(hardwareMap);
         waitForStart();
 
-        robot.encoderDrive(0.6,24,24,24,24,3);
-        robot.encoderDrive(0.6,19.4,-19.4,-19.4,19.4,3);
-        robot.encoderDrive(0.6,72,72,72,72,3);
+        robot.encoderDrive(0.6,26,26,26,26,3);
+        robot.encoderDrive(0.6,-19.4,19.4,19.4,-19.4,3);
+        robot.encoderDrive(0.6,-72,-72,-72,-72,3);
         robot.setArmPosition(170);
         robot.arm.setPower(0.5);
         robot.wrist.setPosition(0.43);
         robot.leftGrip.setPosition(0.5);
         robot.rightGrip.setPosition(0.5);
-        robot.encoderDrive(0.6,24,-24,24,-24,3);
-        robot.encoderDrive(0.6,24,24,24,24,3);
+        robot.setArmPosition(0);
+        robot.encoderDrive(0.6,-24,24,-24,24,3);
+        robot.encoderDrive(0.6,-24,-24,-24,-24,3);
     }
 
 }
