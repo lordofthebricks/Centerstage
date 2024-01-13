3c22b87565e3e6e70d0862527d6094775a001209
aidan7039 <coolgamer17856@gmail.com>
2024-01-13 16:55:16 -0500

    autonomouse for arm and wrist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmMovementBlue1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmMovementBlue1.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 @Autonomous
 public class ArmMovementBlue1 extends LinearOpMode {
 
@@ -11,22 +13,54 @@ public class ArmMovementBlue1 extends LinearOpMode {
 
         hardware robot = new hardware(this);
         robot.init(hardwareMap);
+        double actualDistance = 0;
+        double desiredDistance = 10;
         waitForStart();
 
         robot.encoderDrive(0.6,28,28,28,28,3);
         robot.wrist.setPosition(0.55);
         robot.leftGrip.setPower(-1);
-        sleep(2000);
+        sleep(4000);
+        robot.leftGrip.setPower(0);
+        sleep(1000);
         robot.wrist.setPosition(0.43);
-        robot.encoderDrive(0.6,-19.4,19.4,19.4,-19.4,3);
-        robot.encoderDrive(0.6,-85,-85,-85,-85,3);
+        sleep(1000);
+        robot.encoderDrive(0.6,6,6,6,6,3);
+        robot.encoderDrive(0.6,-20.5,20.5,20.5,-20.5,3);
+//        robot.encoderDrive(0.6,-30,-30,-30,-30,3);
+        actualDistance = robot.distance.getDistance(DistanceUnit.INCH);
+        while (actualDistance > desiredDistance){
+
+            double boardApproachMovement = actualDistance - desiredDistance;
+
+            boardApproachMovement = -boardApproachMovement;
+
+            robot.encoderDrive(0.7, boardApproachMovement, boardApproachMovement, boardApproachMovement, boardApproachMovement, 10);
+
+            actualDistance = robot.distance.getDistance(DistanceUnit.INCH);
+        }
+        desiredDistance = 8;
+
         robot.setArmPosition(170);
-        robot.arm.setPower(0.5);
         robot.wrist.setPosition(0.43);
+        while (actualDistance > desiredDistance){
+
+            double boardApproachMovement = actualDistance - desiredDistance;
+
+            boardApproachMovement = -boardApproachMovement;
+
+            robot.encoderDrive(0.7, boardApproachMovement, boardApproachMovement, boardApproachMovement, boardApproachMovement, 4);
+
+            actualDistance = robot.distance.getDistance(DistanceUnit.INCH);
+        }
+
+
         robot.rightGrip.setPower(-1);
+        sleep(2000);
+        robot.rightGrip.setPower(0);
         robot.setArmPosition(0);
         robot.encoderDrive(0.6,-24,24,-24,24,3);
-        robot.encoderDrive(0.6,-24,-24,-24,-24,3);
+        robot.encoderDrive(0.6,-10,-10,-10,-10,3);
     }
 
 }
