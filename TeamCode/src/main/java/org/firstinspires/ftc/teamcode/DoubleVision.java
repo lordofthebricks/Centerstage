d558bf9b7f605e09650547f03267097bd298c669
victor <therealpenguinolo@gmail.com>
2024-01-25 18:26:33 -0500

    added custom opencv processer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleVision.java
@@ -61,9 +61,7 @@ public class DoubleVision {
     // this is used when uploading models directly to the RC using the model upload interface.
 
     // Define the labels recognized in the model for TFOD (must be in training order!)
-    public static final String[] PIXLABEL = {
-            "Pixel",
-    };
+
     private static final String[] LABELS = {
             "RedEl",
             "BlueEl"
@@ -156,9 +154,6 @@ public class DoubleVision {
                 myOpMode.telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
                 myOpMode.telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
                 myOpMode.telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
-            } else {
-//                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-//                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
             }
         }   // end for() loop
 
