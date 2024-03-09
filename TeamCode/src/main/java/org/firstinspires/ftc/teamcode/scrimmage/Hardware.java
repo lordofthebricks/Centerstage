fbb8fe1ce4d53a550e3c3f164592903bcc6a85a0
Noah <pokemonmanboy@yahoo.com>
2024-03-09 17:12:39 -0500

    hio

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrimmage/Hardware.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.scrimmage;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Hardware {
+
+    public void init (HardwareMap hwMp) {
+
+    }
+}
