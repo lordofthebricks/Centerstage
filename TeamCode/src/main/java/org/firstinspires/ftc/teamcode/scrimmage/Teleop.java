55984880191929b46ff94940e611c2183a48b375
Noah <pokemonmanboy@yahoo.com>
2024-03-09 17:26:25 -0500

    hio

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrimmage/Teleop.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.scrimmage;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Teleop  extends OpMode {
+
+    Hardware robot = new Hardware();
+
+    @Override
+    public void loop () {
+
+    }
+
+    @Override
+    public void init () {
+
+    }
+}
