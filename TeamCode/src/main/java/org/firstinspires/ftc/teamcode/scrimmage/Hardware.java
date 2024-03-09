04deda15efcd5345b23518442c92b5ef85e6128f
aidan7039 <coolgamer17856@gmail.com>
2024-03-09 17:40:48 -0500

    hio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrimmage/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrimmage/Hardware.java
@@ -1,10 +1,19 @@
 package org.firstinspires.ftc.teamcode.scrimmage;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Hardware {
 
+    public DcMotor FrontLeft;
+    public DcMotor BackLeft;
+    public DcMotor FrontRight;
+    public DcMotor BackRight;
     public void init (HardwareMap hwMp) {
 
+     FrontLeft = hwMp.get(DcMotor.class,"frontLeft");
+     BackLeft = hwMp.get(DcMotor.class,"backLeft");
+     FrontRight = hwMp.get(DcMotor.class,"frontRight");
+     BackRight = hwMp.get(DcMotor.class,"backRight");
     }
 }
