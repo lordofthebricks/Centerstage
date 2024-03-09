624e3ab3a559c5a9145b5687cc55d708dd52dfb5
aidan7039 <coolgamer17856@gmail.com>
2024-03-09 17:47:39 -0500

    hio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrimmage/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrimmage/Hardware.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.scrimmage;
 
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -15,5 +16,11 @@ public class Hardware {
      BackLeft = hwMp.get(DcMotor.class,"backLeft");
      FrontRight = hwMp.get(DcMotor.class,"frontRight");
      BackRight = hwMp.get(DcMotor.class,"backRight");
+
+     FrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+     BackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+     FrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+     BackRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
     }
 }
